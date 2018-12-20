0d28b1f2048a032bd89543e2d6548159429fd65b
ythvg <446339431@qq.com>
2018-12-20 20:37:54 +0800

    add screen && ship

---
+++ install_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install python3-dev mercurial
+sudo apt-get install libsdl-image1.2-dev libsdl2-dev libsdl-ttf2.0-dev
+sudo apt-get install libsdl-mixer1.2-dev libportmidi-dev
+sudo apt-get install libswscale-dev libsmpeg-dev libavformat-dev libvcode-dev
+sudo apt-get install python-numpy
+python3 -m pip install -U pygame --user
