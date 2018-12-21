48b14cfa80d16ce7c4f7c0482651f56de624bddd
ythvg <446339431@qq.com>
2018-12-21 11:38:59 +0800

    fly the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
@@ -13,15 +14,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     bg_color = (230, 230, 230)
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
 
